5a517e354dd342f2e16e43f02979ea7b36e99713
tusharinfosun <mounika.yeligeti@gmail.com>
2023-02-03 15:11:35 +0530

    changes in all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { SupportComponent } from './support/support.component';
+import { TeamComponent } from './team/team.component';
+import { AboutComponent } from './about/about.component';
 
 
 
@@ -12,6 +16,10 @@ const routes: Routes = [
 
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    {path:'service',component:ServiceComponent},
+    {path:'support',component:SupportComponent},
+    {path:'team',component:TeamComponent},
+    {path:'about',component:AboutComponent}
  
 
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { RoutingModule } from '../app/app-routing.module';
+import { AboutComponent } from './about/about.component';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ServiceComponent } from './service/service.component';
+import { SupportComponent } from './support/support.component';
+import { TeamComponent } from './team/team.component';
 
 
 
 @NgModule({
   imports:      [ BrowserModule, BrowserAnimationsModule, RoutingModule ],
-  declarations: [ AppComponent,HeaderComponent,FooterComponent],
+  declarations: [ AppComponent,HeaderComponent,FooterComponent,AboutComponent,ServiceComponent,SupportComponent,TeamComponent],
   bootstrap:    [ AppComponent ],
  
 })
